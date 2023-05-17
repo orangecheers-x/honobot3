bb47a7474240c0bcaba2052de2507275241ae106
Orange Cheers <orange_cheers@outlook.com>
2023-05-17 13:08:54 +0800

    load each plugin separately

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 
 import nonebot
 from nonebot.adapters.mirai2 import Adapter as MIRAI2Adapter
+import pathlib
 
 # 初始化 NoneBot
 nonebot.init()
@@ -14,7 +15,9 @@ driver.register_adapter(MIRAI2Adapter)
 # 在这里加载插件
 nonebot.load_builtin_plugins("echo")  # 内置插件
 # nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
-nonebot.load_plugins("plugins")
+nonebot.load_plugin(pathlib.Path("plugins/gpt"))
+nonebot.load_plugin(pathlib.Path("plugins/pythonrunner"))
+nonebot.load_plugin(pathlib.Path("plugins/send"))
 
 if __name__ == "__main__":
     nonebot.run()
